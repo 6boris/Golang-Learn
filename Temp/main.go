88dfc48793ed7347feef39db2f412005dd9c3e07
Kyle Liu <kylesliu@outlook.com>
2018-10-17 17:30:25 +0800

    :cat: Daily Update

--- Temp/main.go
+++ Temp/main.go
@@ -1,18 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"github.com/kylesliu/gonp/Stack"
-)
+import "fmt"
 
 func main() {
-	s := Stack.GetMinStack()
-	fmt.Println(s.Top())
-	fmt.Println(s.IsEmpty())
-	s.Push(1)
-	s.Push(2)
-	s.Push(3)
-	fmt.Println(s.Top())
-	fmt.Println(s.Pop())
-	fmt.Println(s.Top())
+	fmt.Println(GetPage(1, 6, 53))
+}
+
+func GetPage(left, mid, right int) [7]int {
+	arr := [7]int{}
+	MaxWidth := 3
+
+	i, j, count := mid+1, 4, 0
+	for i <= right && count < MaxWidth {
+		arr[j] = i
+		i++
+		count++
+	}
+
+	j, j = 0, mid-1
+	for i >= left && j < MaxWidth {
+
+		i--
+		j++
+	}
+
+	return arr
 }
