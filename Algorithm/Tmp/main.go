b721a6c2f6edae2cbb04146c62e8599e2fadd8e2
Kyle Liu <kylesliu@outlook.com>
2018-11-21 22:30:27 +0800

    Daily update

--- Algorithm/Tmp/main.go
+++ Algorithm/Tmp/main.go
@@ -2,14 +2,46 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
+	"github.com/astaxie/beego/logs"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
-func main() {
-	s := "Kyel刘家"
+var (
+	key []byte = []byte("Hello World！This is secret!")
+)
 
-	for i, ch := range s {
-		fmt.Printf("%d %X\n", i, ch)
+// 产生json web token
+func GenToken() string {
+	claims := &jwt.StandardClaims{
+		NotBefore: int64(time.Now().Unix()),
+		ExpiresAt: int64(time.Now().Unix() + 1000),
+		Issuer:    "Bitch",
 	}
-	fmt.Println(utf8.RuneCountInString(s))
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(key)
+	if err != nil {
+		logs.Error(err)
+		return ""
+	}
+	return ss
+}
+
+// 校验token是否有效
+func CheckToken(token string) bool {
+	_, err := jwt.Parse(token, func(*jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+	if err != nil {
+		fmt.Println("parase with claims failed.", err)
+		return false
+	}
+	return true
+}
+func main() {
+	token := GenToken()
+	fmt.Println(token)
+
+	fmt.Println(CheckToken(token))
 }
--- Temp/tmp1.go
+++ Temp/tmp1.go
@@ -1,20 +1,80 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var db *gorm.DB
+
+func init() {
+	//open a db connection
+	var err error
+	db, err = gorm.Open("mysql", "root:@/demo?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	//Migrate the schema
+	db.AutoMigrate(&todoModel{})
+}
+
+func main() {
+
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1/todos")
+	{
+		v1.POST("/", createTodo)
+		v1.GET("/", fetchAllTodo)
+		v1.GET("/:id", fetchSingleTodo)
+		v1.PUT("/:id", updateTodo)
+		v1.DELETE("/:id", deleteTodo)
+	}
+	router.Run()
+
+}
+
+type (
+	// todoModel describes a todoModel type
+	todoModel struct {
+		gorm.Model
+		Title     string `json:"title"`
+		Completed int    `json:"completed"`
+	}
+
+	// transformedTodo represents a formatted todo
+	transformedTodo struct {
+		ID        uint   `json:"id"`
+		Title     string `json:"title"`
+		Completed bool   `json:"completed"`
+	}
+)
+
+// createTodo add a new todo
+func createTodo(c *gin.Context) {
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
+	db.Save(&todo)
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID})
+}
 
 // fetchAllTodo fetch all todos
 func fetchAllTodo(c *gin.Context) {
 	var todos []todoModel
 	var _todos []transformedTodo
+
 	db.Find(&todos)
+
 	if len(todos) <= 0 {
 		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
+
 	//transforms the todos for building a good response
 	for _, item := range todos {
 		completed := false
@@ -32,17 +92,21 @@ func fetchAllTodo(c *gin.Context) {
 func fetchSingleTodo(c *gin.Context) {
 	var todo todoModel
 	todoID := c.Param("id")
+
 	db.First(&todo, todoID)
+
 	if todo.ID == 0 {
 		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
+
 	completed := false
 	if todo.Completed == 1 {
 		completed = true
 	} else {
 		completed = false
 	}
+
 	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
 }
@@ -51,11 +115,14 @@ func fetchSingleTodo(c *gin.Context) {
 func updateTodo(c *gin.Context) {
 	var todo todoModel
 	todoID := c.Param("id")
+
 	db.First(&todo, todoID)
+
 	if todo.ID == 0 {
 		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
+
 	db.Model(&todo).Update("title", c.PostForm("title"))
 	completed, _ := strconv.Atoi(c.PostForm("completed"))
 	db.Model(&todo).Update("completed", completed)
@@ -66,11 +133,14 @@ func updateTodo(c *gin.Context) {
 func deleteTodo(c *gin.Context) {
 	var todo todoModel
 	todoID := c.Param("id")
+
 	db.First(&todo, todoID)
+
 	if todo.ID == 0 {
 		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
+
 	db.Delete(&todo)
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"})
 }
